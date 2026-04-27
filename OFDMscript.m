2f20a9546900b898ba5741f20670ebc752cc7817
Reece Harrison <reeceharrison9878@gmail.com>
2026-04-27 12:44:10 +1000

    add basic matlab script with only basic setup

---
+++ OFDMscript.m
@@ -0,0 +1,10 @@
+%% Project Script for ECE3141 Friday 12pm D- OFDM 
+% Hamish Cranston & Reece Harrison
+
+%% Setup
+clc;clear all;close all;
+
+
+%% Parameters
+N = 10; % [] number of subcarriers
+T = 1; % [ms] symbol period
